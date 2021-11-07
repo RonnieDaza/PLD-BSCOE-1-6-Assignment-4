c68662e038ecc885dfc513b44d3333e80daa60ce
unknown <daza.ronnie.09212002@gmail.com>
2021-11-07 18:15:13 +0800

    Ask for input of name Commit

--- Assignment 4 - Part 1.py
+++ Assignment 4 - Part 1.py
@@ -1,3 +1,6 @@
 # Create a program that will ask for name, age, and address.
 # Display those details in the following format.
 # Hi, my name is __________. I am __________ years old and I live in __________.
+
+def getNameAgeAddress():
+    _name = input("Enter your name: ")
