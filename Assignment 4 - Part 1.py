e42f3f91f23851901f1032b3a5404391bf1f4e9f
unknown <daza.ronnie.09212002@gmail.com>
2021-11-07 18:11:18 +0800

    Ask for name, age, and address Commit

---
+++ Assignment 4 - Part 1.py
@@ -0,0 +1,13 @@
+# Create a program that will ask for name, age, and address.
+# Display those details in the following format.
+# Hi, my name is __________. I am __________ years old and I live in __________.
+
+def getNameAgeAddress():
+    _name = input("Enter your name: ")
+    _age = int(input("Enter your age: "))
+    _address = input("Enter your address: ")
+    return _name, _age, _address
+
+# Steps
+# 1. Ask for name, age, and address.
+name, age, address = getNameAgeAddress()
