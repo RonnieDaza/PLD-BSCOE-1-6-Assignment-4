faf06e35a83f3ca4cbfe4422b5e117f56a8871f1
unknown <daza.ronnie.09212002@gmail.com>
2021-11-07 18:27:12 +0800

    Add input of how many apples you want to buy Commit

---
+++ Assignment 4 - Part 2.py
@@ -0,0 +1,7 @@
+# Create a program that will ask how many apples and oranges you want to buy.
+# Display the total amount you need to pay if the apple price is 20 pesos and the orange price is 25 pesos.
+# Display the output in the following format.
+# The total amount is __________.
+
+def getTotal():
+    _appleQuestion = int(input("How many apples you want to buy? "))
