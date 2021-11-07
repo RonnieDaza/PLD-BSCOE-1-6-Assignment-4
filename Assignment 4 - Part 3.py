3bb900e57eb0351ad211f8df2a285ba36926d985
unknown <daza.ronnie.09212002@gmail.com>
2021-11-07 18:53:23 +0800

    Ask for input of the amount of money Commit

---
+++ Assignment 4 - Part 3.py
@@ -0,0 +1,7 @@
+# Create a program which you will enter the amount of money you have, it will also ask for the price of an apple.
+# Display the maximum number of apples that you can buy and the remaining money that you will have.
+# Display the output in the following format.
+# You can buy __________ apples and your change is __________ pesos.
+
+def getAppleQuantityMoneyLeft():
+    _money = int(input("Enter the amount of money: "))
