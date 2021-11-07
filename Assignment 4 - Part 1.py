0753ca7c1942eb4930beaf711918b349c7276331
unknown <daza.ronnie.09212002@gmail.com>
2021-11-07 18:17:01 +0800

    Return _name, _age, _address Commit

--- Assignment 4 - Part 1.py
+++ Assignment 4 - Part 1.py
@@ -6,3 +6,4 @@ def getNameAgeAddress():
     _name = input("Enter your name: ")
     _age = int(input("Enter your age: "))
     _address = input("Enter your address: ")
+    return _name, _age, _address
